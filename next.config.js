5514e777856d90c41a44f6233864ed28b7141a9e
imostafa <mostafa.osivand@gmail.com>
2024-08-24 03:36:29 -1200

    update next config file

--- next.config.js
+++ next.config.js
@@ -1,32 +1,32 @@
 const { i18n } = require('./next-i18next.config');
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   pwa: {
-//     disable: process.env.NODE_ENV === 'development',
-//     dest: 'public',
-//     runtimeCaching,
-//   },
-//   i18n,
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-// });
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    runtimeCaching,
+  },
+  i18n,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+});
 
-const nextConfig = {
-  output: 'export',
+// const nextConfig = {
+//   output: 'export',
  
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//   // trailingSlash: true,
  
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//   // skipTrailingSlashRedirect: true,
  
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
+//   // Optional: Change the output directory `out` -> `dist`
+//   // distDir: 'dist',
+// }
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
 
 
